e93ee8ee0bfdc2053b18fe833bb4227c7ea8be16
gmpz <tangpanitan2539@gmail.com>
2025-10-24 21:57:38 +0700

    update basket-list
    update basket-list

--- components/basket/basket-list.tsx
+++ components/basket/basket-list.tsx
@@ -1,10 +1,10 @@
+'use client';
 import React from "react";
 import BasketItem from "./basket-item";
 import { useBasket } from "@/context/basket-context";
 
 const BasketList = () => {
   const { basket } = useBasket();
-  console.log("basket :", basket);
   
   return (
     <div className="grid flex-1 auto-rows-min px-4 overflow-y-auto scrollbar-hide">
